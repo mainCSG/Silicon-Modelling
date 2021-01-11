bb635b21c87f6159d2b264c6b85fd2a17368ba8d
aaran <aaranyaalexander@gmail.com>
2021-01-11 14:55:36 -0500

    Restructured input checks, simplified effHO

--- qudipy/qutils/hamiltonian.py
+++ qudipy/qutils/hamiltonian.py
@@ -1,88 +1,128 @@
 """
-Hamiltonian generalization class
+Hamiltonian matrix functions
 
-author: aaranyaalex
+@author: aaranyaalex
 """
 import numpy as np
 import qudipy.potential as pot
 from qudipy.qutils.solvers import build_1DSE_hamiltonian, build_2DSE_hamiltonian
 from scipy.sparse.linalg import eigs
+from scipy.linalg import block_diag
 from types import SimpleNamespace
 
-class HamGen:
 
-    def ham_interp(self, i_params):
-        params = self.extract_dict(i_params)
-        compat_types = ['RealSpace', 'effOrbital', 'effSpin']
-        compat_params = [['PotInterp', 'V'], ['TC', 'Eps', 'OSplit'], ['TC', 'Eps', 'VPhase', 'VSplit', 'Ez']]
+def ham_interp(i_params):
+    params = extract_dict(i_params)
+    H_RS = None
+    effH_O = None
+    effH_S = None
 
-        if not hasattr(params, 'HamType'):
-            # Assign a HamType based on the available parameter attributes
-            params.HamType = [compat_types[compat_params.index(row)] for row in compat_params
-                              if(set(row).issubset(set(params.__dict__)))]
-            # Raise error if no HamType can be formed
-            if not params.HamType:
-                raise ValueError('Supplied parameters are insufficient to form supported Hamiltonians')
-        else:
-            if 'RealSpace' in params.HamType:
-                potential = params.PotInterp(params.V)
-                gparams = pot.GridParameters(params.PotInterp.xcoords, potential=potential)
-                if gparams.grid_type == '1D':
-                    H_RS = build_1DSE_hamiltonian(params.PotInterp.constants, gparams)
-                elif gparams.grid_type == '2D':
-                    H_RS = build_2DSE_hamiltonian(params.PotInterp.constants, gparams)
+    if 'RealSpace' in params.HamType:
+        potential = params.PotInterp(params.V)
+        gparams = pot.GridParameters(params.PotInterp.xcoords, potential=potential)
 
-            if 'effOrbital' in params.HamType:
-                n_dots = min(len(params.TC)+1, len(params.Eps), len(params.OSplit))
+        if gparams.grid_type == '1D':
+            H_RS = build_1DSE_hamiltonian(params.PotInterp.constants, gparams)
+        elif gparams.grid_type == '2D':
+            H_RS = build_2DSE_hamiltonian(params.PotInterp.constants, gparams)
 
-                if n_dots < max(len(params.TC)+1, len(params.Eps), len(params.OSplit)):
-                    params.TC = params.TC[:n_dots-1]
-                    params.Eps = params.Eps[:n_dots]
-                    params.OSplit = params.OSplit[:n_dots]
-                    print(f'Supplied data inconsistent, calculating H for a {n_dots}QD-system')
+    if 'effOrbital' in params.HamType:
+        n_dots = min(len(params.TC)+1, len(params.Eps), len(params.OSplit))
 
-                # form diagonals from parameters
-                d_0 = np.insert(np.array(params.OSplit)+np.array(params.Eps), slice(None, None, 1), params.Eps)
-                d_1 = np.insert(np.array(params.TC), slice(None, None, 1), np.zeros(n_dots+1))
-                d_2 = np.repeat(np.array(params.TC), 2)
-                d_3 = np.insert(np.zeros(n_dots-1), slice(None, None, 1), np.array(params.TC))
+        if n_dots < max(len(params.TC)+1, len(params.Eps), len(params.OSplit)):
+            params.TC = params.TC[:n_dots-1]
+            params.Eps = params.Eps[:n_dots]
+            params.OSplit = params.OSplit[:n_dots]
+            print(f'Supplied data inconsistent, calculating H for a {n_dots}QD-system')
 
-                effH_O = np.diag(d_0) + np.diag(d_1, 1) + np.diag(d_1, -1) + np.diag(d_2, 2) + np.diag(d_2, -2) + \
-                         np.diag(d_3, 3) + np.diag(d_3, -3)
+        # form Hamiltonian using block diagonals
+        blocks = np.multiply.outer(params.TC, np.ones((2, 2), int))
+        off = np.empty((0, 2), int)
+        effH_O = block_diag(off.T, *blocks, off) + block_diag(off, *blocks, off.T) + \
+                 np.diag(np.insert(np.array(params.OSplit)+np.array(params.Eps), slice(None, None, 1), params.Eps))
 
-            if 'effSpin' in params.HamType:
+    if 'effSpin' in params.HamType:
+        n_dots = min(len(params.TC) + 1, len(params.Eps), len(params.VSplit))
 
-            else: # throw error, inputted type not compatible
+        if n_dots < max(len(params.TC) + 1, len(params.Eps), len(params.VSplit)):
+           params.TC = params.TC[:n_dots - 1]
+           params.Eps = params.Eps[:n_dots]
+           params.VSplit = params.VSplit[:n_dots]
+           params.Eta = params.Eta[:n_dots]
+           print(f'Supplied data inconsistent, calculating H for a {n_dots}QD-system')
 
-        return H_RS, effH_O, effH_S
+        # Two level operators
+        Ax = np.array([[0, 1], [1, 0]], dtype=complex)
+        Ay = np.array([[0, -1j], [1j, 0]], dtype=complex)
+        Az = np.array([[1, 0], [0, -1]], dtype=complex)
+        Ao = np.eye(2)
+        A_p = 0.5*(Ax + 1j * Ay)
+        A_m = 0.5*(Ax - 1j * Ay)
+        # 3D arrays storing k operators for all dots
+        k_d = np.zeros((n_dots, n_dots, n_dots), dtype=complex)
+        k_d[np.diag_indices(n_dots, ndim=3)] = np.ones((n_dots))
+        k_x = np.array([np.roll(np.pad(Ax, ((0,n_dots-2), (0,n_dots-2))), (n, n), axis=(0, 1)) for n in range(n_dots-1)])
+        k_y = np.array([np.roll(np.pad(Ay, ((0,n_dots-2), (0,n_dots-2))), (n, n), axis=(0, 1)) for n in range(n_dots-1)])
+        k_z = np.array([np.roll(np.pad(Az, ((0,n_dots-2), (0,n_dots-2))), (n, n), axis=(0, 1)) for n in range(n_dots-1)])
+        k_0 = np.eye(n_dots)
 
-    def extract_dict(self, params):
-        # compatible parameters to create hamiltonians
-        compat = ['V', 'PotInterp', 'HamType', 'TC', 'Eps', 'OSplit', 'VPhase', 'VSplit', 'Ez']
-        # check if parameters in dictionary are formatted properly
-        if all(item in list(params.keys()) for item in compat):
-            # send dictionary elements to variable
-            extracted_data = SimpleNamespace(**params)
-        else:
-            # show user the improperly formatted keys
-            invalid_keys = np.setdiff1d(list(params.keys()), compat)
+        effH_S = np.sum(np.kron(np.kron(k_d*params.Eps, Ao), Ao) + np.kron(np.kron(k_d*params.VSplit, A_p), Ao) + \
+                       np.kron(np.kron(k_d*params.Vplit, A_m), Ao), axis=2) + \
+                np.sum(np.kron(np.kron(k_x*params.TC, Ao), Ao) + np.kron(np.kron(k_z*params.Eta[0], Ao), Ax) + \
+                       np.kron(np.kron(k_y * params.Eta[0], Ao), Ax), axis=2) + \
+                np.kron(np.kron(k_0 * params.Ez, Ao), Az)
+
+    return H_RS, effH_O, effH_S
+
+
+def extract_dict(i_params):
+    compat_types = ['RealSpace', 'effOrbital', 'effSpin']
+    compat_params = [['PotInterp', 'V'], ['TC', 'Eps', 'OSplit'], ['TC', 'Eps', 'VSplit', 'Ez', 'Eta']]
+    params = SimpleNamespace(**i_params)
+
+    if not hasattr(params, 'HamType'):
+        # Assign a HamType based on the available parameter attributes
+        params.HamType = [compat_types[compat_params.index(row)] for row in compat_params
+                          if(set(row).issubset(set(params.__dict__)))]
+        # Raise error if no HamType can be formed
+        if not params.HamType:
+            raise ValueError('Supplied parameters are insufficient to form supported Hamiltonians')
+    else:
+        try:
+            # find total parameters needed to calculated user-inputted HamType(s)
+            if type(params.HamType) is str:
+                total_params = compat_params[compat_types.index(params.HamType)]
+            else:
+                total_params = sum([compat_params[compat_types.index(item)] for item in params.HamType], [])
+        except ValueError:
+            # error if user-inputted HamType unsupported
+            raise ValueError(f'Hamiltonian type {params.HamType} is invalid.\nCompatible types are:\n' +
+                             f'{compat_types}')
+
+        # check if inputted parameters correspond to total params needed
+        if not all(item in list(i_params.keys()) for item in total_params):
+            # show user the improperly formatted / missing keys
+            invalid_keys = np.setdiff1d(total_params, list(i_params.keys()))
             raise ValueError(f'Supplied dictionary keys {invalid_keys} are ' +
-                             'invalid.\nCompatible keys are:\n' +
-                             f'{compat}')
-        return extracted_data
+                             f'missing or invalid for the {params.HamType} Hamiltonian(s).\nNecessary keys are:\n' +
+                             f'{total_params}')
+    return params
 
-    def eigs(self, nsols=1):
-        e_ens, e_vecs = eigs(self.ham, k=nsols, sigma=self.gparams.potential.min())
-        idx = e_ens.argsort()
-        e_ens = e_ens[idx]
-        e_vecs = e_vecs[:, idx]
 
-        return e_ens, e_vecs
+def eigens(ham_array, params, nsols=1):
+    potential = params['PotInterp'](params['V'])
+    e_ens, e_vecs = eigs(ham_array, k=nsols, sigma=potential.min())
+    idx = e_ens.argsort()
+    e_ens = e_ens[idx]
+    e_vecs = e_vecs[:, idx]
 
-    def expectation_vals(self, wf1, wf2):
-        wf1 = wf1.reshape(1, -1).conj()
-        wf2 = wf2.reshape(-1, 1)
+    return e_ens, e_vecs
 
-        E = np.dot(wf1, np.dot(self.ham, wf2))
 
-        return E
+def expectation(ham_array, wf1, wf2):
+    wf1 = wf1.reshape(1, -1).conj()
+    wf2 = wf2.reshape(-1, 1)
+
+    E = np.dot(wf1, np.dot(ham_array, wf2))
+
+    return E
