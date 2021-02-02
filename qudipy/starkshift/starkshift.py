67821e2618a8c2cd09c64bb1327b463919360763
bleutooth65 <51833105+bleutooth65@users.noreply.github.com>
2021-02-02 08:43:09 -0500

    Stark shift output is pandas df and mu_2 is in Constants class

--- qudipy/starkshift/starkshift.py
+++ qudipy/starkshift/starkshift.py
@@ -4,18 +4,25 @@ from ..qutils.solvers import solve_schrodinger_eq
 
 # From external libraries
 import numpy as np
+import pandas as pd
 
 class StarkShift:
     def __init__(self, gparams, consts):
         self.gparams = gparams
         self.consts = consts
 
-    def delta_g(self, e_interp, c_vals, wavefuncs=None):
+    def delta_g(self, e_interp, c_vals, c_val_names, wavefuncs=None):
 
         if wavefuncs==None:
             _, wavefuncs = solve_schrodinger_eq(self.consts, self.gparams, n_sols=1)
 
         c_vals_delta_g = []
+
+        if self.consts.mu_2 is not None:
+            mu_2 = self.consts.mu_2
+        else:
+            raise NotImplementedError("The quadratic Stark shift coefficient in this material \
+                                       has not been specified in the constants class")
 
         for c_val in c_vals:
         # Return the potential interpolated (or calculated) at that particular value
@@ -29,7 +36,6 @@ class StarkShift:
                 avg_e = expectation_value(self.gparams, wavefunc, np.square(new_e))
             
                 # Multiply by the ratio that was found in https://doi.org/10.1038/nnano.2014.216
-                mu_2 = 2.2* (1e-9)**2 #(nm^2/V^2)
                 delta_g = mu_2 * avg_e
                 delta_g_list.append(np.real(delta_g))
 
@@ -37,25 +43,10 @@ class StarkShift:
             c_vals_delta_g.append(c_val + delta_g_list)
 
         # Return the calculated value of delta_g
-
-        return np.array(c_vals_delta_g)
-
-    def find_wavefunctions(self, pot_interp, approx_location, c_vals):
-        pass
-        # wavefuncs = []
-        # num_dots = len(approx_location)
-        # initial_v_vec = []
-        # for i in range(len(c_vals)):
-        #     setattr(self, 'c_val_' + str(i) , c_vals[i])
-        #     initial_v_vec.append(min(getattr(self, 'c_val_' + str(i))))
-
-        # pot_interp.
-        # for j in range(num_dots):
-
-
-        #     _, wavefunc = solve_schrodinger_eq(self.consts, self.gparams, n_sols=1)
-
-        # return wavefuncs
+        delta_gs = ['delta_g_' + str(i+1) for i in range(len(wavefuncs))]
+        coulmns = c_val_names + delta_gs
+        df = pd.DataFrame(c_vals_delta_g, columns=coulmns)
+        return df
 
 
 
